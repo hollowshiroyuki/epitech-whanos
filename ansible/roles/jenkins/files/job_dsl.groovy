499ac78f08f4066e6ae3baee64d9bd28bf95c54f
Tino <tino.tempesta@epitech.eu>
2023-02-25 18:57:47 +0100

    Add: Link-project job

--- ansible/roles/jenkins/files/job_dsl.groovy
+++ ansible/roles/jenkins/files/job_dsl.groovy
@@ -6,33 +6,53 @@ folder("Projects") {
     description("Projects")
 }
 
+def languages = ["befunge", "c", "java", "javascript", "python"]
 
-freeStyleJob("Whanos base images/whanos-befunge") {
-    steps {
-        shell('echo whanos-befunge')
+languages.each { lang ->
+    freeStyleJob("Whanos base images/whanos-$lang") {
+        steps {
+            shell("docker build /var/jenkins_home/images/$lang -t whanos-$lang -f /var/jenkins_home/images/$lang/Dockerfile.base")
+        }
     }
 }
 
-freeStyleJob("Whanos base images/whanos-c") {
-    steps {
-        shell('echo whanos-c')
+freeStyleJob("Whanos base images/Build all base images") {
+    description("Build all base images in a beautiful fashion")
+    publishers {
+        downstream(languages.collect{"whanos-$it"})
     }
 }
 
-freeStyleJob("Whanos base images/whanos-java") {
-    steps {
-        shell('echo whanos-java')
+freeStyleJob("link-project") {
+    parameters {
+        stringParam('GIT_NAME', null, 'Git repository owner/repo_name (e.g.: "https://git.sr.ht/~hollow/Homelab")')
+        stringParam('DISPLAY_NAME', null, 'Display name for the job')
     }
-}
-
-freeStyleJob("Whanos base images/whanos-javascript") {
     steps {
-        shell('echo whanos-javascript')
-    }
-}
-
-freeStyleJob("Whanos base images/whanos-python") {
-    steps {
-        shell('echo whanos-python')
+        dsl{
+            text('''freeStyleJob("Projects/${DISPLAY_NAME}") {
+                |  scm {
+                |   git {
+                |    remote {
+                |       credentials("ahha")
+                |       name("origin")
+                |       url("${GIT_NAME}")
+                |    }
+                |   }
+                |  }
+                |  triggers {
+                |    scm("* * * * *")
+                |  }
+                |  wrappers {
+                |    preBuildCleanup {
+                |      includePattern('*')
+                |      deleteDirectories()
+                |    }
+                |  }
+                |  steps {
+                |       shell("/var/jenkins_home/link-project.sh \\"$DISPLAY_NAME\\"")
+                |    }
+                }'''.stripMargin())
+        }
     }
 }
